c5fa667adbec0e8c517ef7c469655b2168f6ba13
Mohammad Omid Taheri <m.omidtaheri@yahoo.com>
2018-11-01 21:15:57 +0330

    AngelsGate V.2 Stable

--- Fog-Client/Android/AngelsGateSdk/app/src/main/java/com/angelsgate/sdk/AngelsGate.java
+++ Fog-Client/Android/AngelsGateSdk/app/src/main/java/com/angelsgate/sdk/AngelsGate.java
@@ -5,7 +5,9 @@ import android.content.Context;
 import com.angelsgate.sdk.AngelsGateNetwork.DecodeResponse;
 import com.angelsgate.sdk.AngelsGateNetwork.EncodeRequest;
 import com.angelsgate.sdk.AngelsGateUtils.AngelGateErroreHandler;
+import com.angelsgate.sdk.AngelsGateUtils.RSACrypt;
 import com.angelsgate.sdk.AngelsGateUtils.RandomUtils;
+import com.angelsgate.sdk.AngelsGateUtils.prefs.AngelGatePreferencesHelper;
 
 import java.io.UnsupportedEncodingException;
 import java.security.GeneralSecurityException;
@@ -32,6 +34,12 @@ public class AngelsGate {
     }
 
 
+    public static String GenPublicKey(Context ctx) {
+        RSACrypt.GenerateRsaKey(ctx);
+        return AngelGatePreferencesHelper.getPublicKeyGenerated(ctx);
+    }
+
+
     public static String DecodeResponse(String encryptedString, String Ssalt, String mainDeviceId, String methodName, Context ctx) throws GeneralSecurityException {
         return DecodeResponse.decode(encryptedString, Ssalt, mainDeviceId, methodName, ctx);
     }
@@ -44,6 +52,11 @@ public class AngelsGate {
 
     public static boolean ErroreHandler(String respose) {
         return AngelGateErroreHandler.ErrorHandler(respose);
+    }
+
+
+    public static boolean StringErroreHandler(String respose) {
+        return AngelGateErroreHandler.StringErrorHandler(respose);
     }
 
 
