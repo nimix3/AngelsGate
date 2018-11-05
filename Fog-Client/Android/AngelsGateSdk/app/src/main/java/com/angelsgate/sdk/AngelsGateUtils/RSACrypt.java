500651a6d3d263529761232bad063e192c9896a0
Mohammad Omid Taheri <m.omidtaheri@yahoo.com>
2018-11-05 22:54:15 +0330

    remove some extra operation and improved

--- Fog-Client/Android/AngelsGateSdk/app/src/main/java/com/angelsgate/sdk/AngelsGateUtils/RSACrypt.java
+++ Fog-Client/Android/AngelsGateSdk/app/src/main/java/com/angelsgate/sdk/AngelsGateUtils/RSACrypt.java
@@ -26,9 +26,6 @@ import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.OAEPParameterSpec;
 import javax.crypto.spec.PSource;
 
-/**
- * Created by om on 7/23/2018.
- */
 
 public class RSACrypt {
 
@@ -44,44 +41,25 @@ public class RSACrypt {
             publicKey = getPublicKey(AngelGateConstants.publicKey);
         }
 
-        //////////////////////
+     
         Cipher oaepFromInit = Cipher.getInstance("RSA/ECB/OAEPPadding");
         OAEPParameterSpec oaepParams = new OAEPParameterSpec("SHA-256", "MGF1", new MGF1ParameterSpec("SHA-1"), PSource.PSpecified.DEFAULT);
         oaepFromInit.init(Cipher.ENCRYPT_MODE, publicKey, oaepParams);
         byte[] encryptedBytes = oaepFromInit.doFinal(plain.getBytes("UTF-8"));
         String encoded = Base64Utils.Base64Encode(encryptedBytes);
-        ////////////////////
+        
 
         return encoded;
     }
 
 
-    public static String RSAEncryptByte(final String plain, Context ctx) throws NoSuchAlgorithmException, NoSuchPaddingException,
-            InvalidKeyException, IllegalBlockSizeException, BadPaddingException, UnsupportedEncodingException {
-
-        PublicKey publicKey;
-
-        if (AngelGatePreferencesHelper.getPublicKeyGenerated(ctx).length() > 0) {
-            publicKey = getPublicKey(AngelGatePreferencesHelper.getPublicKeyGenerated(ctx));
-        } else {
-
-            publicKey = getPublicKey(AngelGateConstants.publicKey);
-        }
-
-
-        Cipher cipher = Cipher.getInstance("RSA/ECB/OAEPwithSHA-256andMGF1Padding");
-        cipher.init(Cipher.ENCRYPT_MODE, publicKey);
-        byte[] encryptedBytes = cipher.doFinal(plain.getBytes("UTF-8"));
-        String encoded = Base64Utils.Base64Encode(encryptedBytes);
-        return encoded;
-    }
 
 
     public static String RSADecryptByte(final byte[] encryptedBytes, String privateKeyString) throws NoSuchAlgorithmException, NoSuchPaddingException,
             InvalidKeyException, IllegalBlockSizeException, BadPaddingException, UnsupportedEncodingException, InvalidAlgorithmParameterException {
 
         PrivateKey privateKey = getPrivateKey(privateKeyString);
-        //////////////////////
+       
         Cipher oaepFromInit = Cipher.getInstance("RSA/ECB/OAEPPadding");
         OAEPParameterSpec oaepParams = new OAEPParameterSpec("SHA-256", "MGF1", new MGF1ParameterSpec("SHA-1"), PSource.PSpecified.DEFAULT);
         oaepFromInit.init(Cipher.DECRYPT_MODE, privateKey, oaepParams);
@@ -92,19 +70,6 @@ public class RSACrypt {
     }
 
 
-    public static String RSADecrypt(final String EncodedText, String privateKeyString) throws NoSuchAlgorithmException, NoSuchPaddingException,
-            InvalidKeyException, IllegalBlockSizeException, BadPaddingException, UnsupportedEncodingException {
-
-        PrivateKey privateKey = getPrivateKey(privateKeyString);
-
-        byte[] encryptedBytes = EncodedText.getBytes("UTF-8");
-
-        Cipher cipher = Cipher.getInstance("RSA/ECB/OAEPwithSHA-256andMGF1Padding");
-        cipher.init(Cipher.DECRYPT_MODE, privateKey);
-        byte[] decryptedBytes = cipher.doFinal(encryptedBytes);
-        String decrypted = new String(decryptedBytes);
-        return decrypted;
-    }
 
     public static PublicKey getPublicKey(String key) {
         try {
@@ -135,26 +100,4 @@ public class RSACrypt {
         return null;
     }
 
-
-    public static void GenerateRsaKey(Context ctx) {
-
-
-        try {
-            KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
-            kpg.initialize(1024); // You can set a different value here
-
-            KeyPair kp = kpg.generateKeyPair();
-            PrivateKey prk = kp.getPrivate();
-            PublicKey puk = kp.getPublic();
-
-
-            AngelGatePreferencesHelper.setPrivateKeyGenerated(Base64Utils.Base64Encode(prk.getEncoded()), ctx);
-            AngelGatePreferencesHelper.setPublicKeyGenerated(Base64Utils.Base64Encode(puk.getEncoded()), ctx);
-
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-
-
-    }
 }
